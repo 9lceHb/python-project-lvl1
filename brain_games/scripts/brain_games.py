0a43232987d39ab1bc17f1edc7d75da2d2c374a3
SERGEI <yasen20008@gmail.com>
2021-08-18 15:36:36 +0000

    hexlet_project part 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import brain_games.cli as cli
+
+
 
 def main():
     print('Welcome to the Brain Games!')
+    name =cli.welcome_user()
+    print(f'Hello, {name}!')
 
 
 if __name__ == '__main__':
